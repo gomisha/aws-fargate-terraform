c1a4c39d824e53dd97b7def9e5c605b405368e18
Misha Rybalov <Misha_Rybalov@symantec.com>
2019-11-14 18:28:41 -0500

    added route tables

--- 1-infrastructure/vpc.tf
+++ 1-infrastructure/vpc.tf
@@ -6,11 +6,15 @@ terraform {
     backend "s3" {}
 }
 
+### VPC
+
 resource "aws_vpc" "production-vpc" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
     tags = {Name="Production-VPC"}
 }
+
+### PUBLIC SUBNETS
 
 resource "aws_subnet" "public-subnet-1" {
   cidr_block = "${var.public_subnet_1_cidr}"
@@ -33,6 +37,8 @@ resource "aws_subnet" "public-subnet-3" {
   tags = {Name="Public-Subnet-3"}
 }
 
+### PRIVATE SUBNETS
+
 resource "aws_subnet" "private-subnet-1" {
   cidr_block = "${var.private_subnet_1_cidr}"
   vpc_id = "${aws_vpc.production-vpc.id}"
@@ -52,4 +58,49 @@ resource "aws_subnet" "private-subnet-3" {
   vpc_id = "${aws_vpc.production-vpc.id}"
   availability_zone = "us-east-1c"
   tags = {Name="Private-Subnet-3"}
+}
+
+### ROUTE TABLES
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = "${aws_vpc.production-vpc.id}"
+  tags = {Name="Public Route Table"}
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = "${aws_vpc.production-vpc.id}"
+  tags = {Name="Private Route Table"}
+}
+
+### ROUTE TABLE ASSOCIATIONS - PUBLIC
+
+resource "aws_route_table_association" "public-route-table-subnet-1-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-1.id}"
+}
+
+resource "aws_route_table_association" "public-route-table-subnet-2-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-2.id}"
+}
+
+resource "aws_route_table_association" "public-route-table-subnet-3-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-3.id}"
+}
+
+### ROUTE TABLE ASSOCIATIONS - PRIVATE
+resource "aws_route_table_association" "private-route-table-subnet-1-association" {
+  route_table_id = "${aws_route_table.private-route-table-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-1}"
+}
+
+resource "aws_route_table_association" "private-route-table-subnet-2-association" {
+  route_table_id = "${aws_route_table.private-route-table-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-2}"
+}
+
+resource "aws_route_table_association" "private-route-table-subnet-3-association" {
+  route_table_id = "${aws_route_table.private-route-table-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-3}"
 }
